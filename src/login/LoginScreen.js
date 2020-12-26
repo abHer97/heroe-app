b26d5561c40850de0fecf9acce2bff4d128e43df
Abel Herrera <ahpard97@gmail.com>
2020-12-25 21:55:43 -0600

    agregados comentarios sobre el uso del historial en rutas, en archivo login/LoginScreen.js

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+   const handleClick = () => {
+      // history.push('/'); agrega la ruta de parametro al historial de navegacion
+      history.replace('/'); //borra el historial e inserta la ruta del parametro, haciendo que no exista historial de navegacion y la ruta otorgada sera la 'unica' visitada
+   };
+
    return (
-      <div>
-         <h1>Login Screen</h1>
+      <div className='container mt-5'>
+         <h1>Login</h1>
+         <hr />
+         <button className='btn btn-primary' onClick={handleClick}>
+            Login
+         </button>
       </div>
    );
 };
