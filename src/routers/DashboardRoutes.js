76f408b422463f933ecfdee97105aefa47e483ad
Abel Herrera <ahpard97@gmail.com>
2020-12-25 21:43:41 -0600

    Primer commit teniendo dos routers y comprobando su correcta funcionalidad

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import { NavBar } from '../components/ui/NavBar';
+import { HeroeScreen } from '../components/heroes/HeroeScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { DcScreen } from '../components/dc/DcScreen';
+
+export const DashboardRoutes = () => {
+   return (
+      <>
+         <NavBar />
+
+         <div>
+            <Switch>
+               <Route exact path='/marvel' component={MarvelScreen} />
+               <Route exact path='/dc' component={DcScreen} />
+               <Route exact path='/heroe/:heroeid' component={HeroeScreen} />
+
+               <Redirect to='/marvel' />
+            </Switch>
+         </div>
+      </>
+   );
+};
