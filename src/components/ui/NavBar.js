9d5a0ba2e791da2967d8495c1a3d11394dc97a93
Abel Herrera <ahpard97@gmail.com>
2021-01-02 22:24:53 -0600

    Implementacion del logout y redireccionamiento al login con uso del history obtenido mediante hook en ves de props

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 export const NavBar = () => {
+   const {
+      user: { name },
+      dispatch
+   } = useContext(AuthContext);
+
+   const history = useHistory(); //este hook permite acceder a la url actual para hacer un redireccionamiento
+   //sin tener que pedir el history mediante props, teniendo asi un componente mas limpio
+
+   const handleLogout = () => {
+      dispatch({
+         type: types.logout
+      });
+
+      history.replace('/login');
+   };
+
    return (
       <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
          <Link className='navbar-brand' to='/'>
-            Asociaciones
+            Asociations
          </Link>
 
          <div className='navbar-collapse'>
@@ -40,14 +58,10 @@ export const NavBar = () => {
 
          <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
             <ul className='navbar-nav ml-auto'>
-               <NavLink
-                  activeClassName='active'
-                  className='nav-item nav-link'
-                  exact
-                  to='/login'
-               >
+               <span className='nav-item nav-link text-info'>{name}</span>
+               <button className='nav-item nav-link btn bg-dark' onClick={handleLogout}>
                   Logout
-               </NavLink>
+               </button>
             </ul>
          </div>
       </nav>
--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
+import { types } from '../types/types';
 
 export const LoginScreen = ({ history }) => {
+   const { dispatch } = useContext(AuthContext);
+
    const handleClick = () => {
       // history.push('/'); agrega la ruta de parametro al historial de navegacion
-      history.replace('/'); //borra el historial e inserta la ruta del parametro, haciendo que no exista historial de navegacion y la ruta otorgada sera la 'unica' visitada
+      // history.replace('/'); //borra el historial e inserta la ruta del parametro, haciendo que no exista historial de navegacion y la ruta otorgada sera la 'unica' visitada
+      dispatch({
+         type: types.login,
+         payload: {
+            name: 'Fernando'
+         }
+      });
+
+      history.replace('/');
    };
 
    return (
